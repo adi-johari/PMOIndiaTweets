490dfc94c8039db3dd942b3b1cd1751cd5e3a634
Aditya <aditya.johari@gmail.com>
2019-08-27 15:27:04 +0530

    third commit

--- PMOIndiaTweetAnalysis.R
+++ PMOIndiaTweetAnalysis.R
@@ -6,3 +6,49 @@ library(tm)
 corpus <- iconv(PMOTweets$Text, to = "utf-8")
 corpus <- Corpus(VectorSource(corpus))
 inspect(corpus[1:5])
+
+#Clean Data
+corpus<- tm_map(corpus, tolower)
+corpus<- tm_map(corpus, removePunctuation)
+corpus<- tm_map(corpus, removeNumbers)
+cleanset<- tm_map(corpus, removeWords, stopwords('english'))
+
+removeURL <- function(x) gsub('http[[:alnum:]]*', '', x)
+cleanset <- tm_map(cleanset, content_transformer(removeURL))
+
+removeN <- function(x) gsub('\\n', '', x)
+cleanset <- tm_map(cleanset, content_transformer(removeN))
+inspect(cleanset[1:5])
+
+library(stringi)
+a<- c('\xe2�\xe2�\xe3')
+b<- stri_trans_general(("\xe2�\xe2�\xe3", "latin-ascii"))
+
+
+removeS <- function(x) gsub('ã¢â‚¬â„¢s', '', x)
+cleanset <- tm_map(cleanset, content_transformer(removeS))
+
+
+
+removeJabber <- function(x) gsub('*xe2*', '', x)
+cleanset <- tm_map(cleanset, content_transformer(removeJabber))
+
+cleanset<- tm_map(cleanset, stripWhitespace)
+inspect(cleanset[1:10])
+
+
+#Term Document Matrix
+tdm<- TermDocumentMatrix(cleanset)
+tdm
+tdm<- as.matrix(tdm)
+tdm[1:10, 1:20]
+
+#BarPlot
+w<- rowSums(tdm)
+w<- subset(w, w>=15)
+w
+
+
+
+
+
