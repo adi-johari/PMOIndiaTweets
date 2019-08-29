efe40d01e1397a7a61ed3cbbc6952d997a799724
Aditya <aditya.johari@gmail.com>
2019-08-29 17:25:30 +0530

    final commit

--- PMOIndiaTweetAnalysis.R
+++ PMOIndiaTweetAnalysis.R
@@ -3,7 +3,7 @@ PMOTweets <- read.csv(file.choose(), header = T)
 
 #Build corpus
 library(tm)
-corpus <- iconv(PMOTweets$Text, to = "utf-8")
+corpus <- iconv(PMOTweets$Text, from = "utf-8", to= "ASCII//TRANSLIT")
 corpus <- Corpus(VectorSource(corpus))
 inspect(corpus[1:5])
 
@@ -23,6 +23,25 @@ inspect(cleanset[1:5])
 removeS <- function(x) gsub('ã¢â‚¬â„¢s', '', x)
 cleanset <- tm_map(cleanset, content_transformer(removeS))
 
+cleanset <- tm_map(cleanset, removeWords, c('will', 'since', 'see', 'meet', 'also', 'many', 'can', 'look', 'just', 'much', 'two', 'make', 'towards', 'called', 'talks', 'live', 'due', 'like', 'now', 'need', 'way', 'ago', 'tomorrow', 'let', 'working', 'want', 'earlier', 'met', 'day', 'help', 'sri', 'parts', 'get', 'done', 'lakh', 'days', 'may', 'came', 'never', 'see', 'meet', 'said', 'know', 'become' ))
+cleanset <- tm_map(cleanset, removeWords, c('give', 'last', 'work', 'made', 'create', 'must', 'come', 'via', 'thoughts' ))
+cleanset <- tm_map(cleanset, removeWords, c('lost', 'visit', 'lives', 'take', 'today', 'every', 'year', 'even', 'shri', 'worked','long', 'best', 'think', ))
+
+cleanset <- tm_map(cleanset, gsub, pattern = 'narendramodi', replacement = 'modi')
+cleanset <- tm_map(cleanset, gsub, pattern = 'narendramodis', replacement = 'modi')
+cleanset <- tm_map(cleanset, gsub, pattern = 'namo', replacement = 'modi')
+cleanset <- tm_map(cleanset, gsub, pattern = 'talks', replacement = 'ties')
+cleanset <- tm_map(cleanset, gsub, pattern = 'coming', replacement = 'come')
+cleanset <- tm_map(cleanset, gsub, pattern = 'uttar', replacement = 'uttarpradesh')
+cleanset <- tm_map(cleanset, gsub, pattern = 'pradesh', replacement = 'uttarpradesh')
+cleanset <- tm_map(cleanset, gsub, pattern = 'cleanliness', replacement = 'clean')
+cleanset <- tm_map(cleanset, gsub, pattern = 'gandhi', replacement = 'bapu')
+cleanset <- tm_map(cleanset, gsub, pattern = 'indias', replacement = 'india')
+cleanset <- tm_map(cleanset, gsub, pattern = 'indians', replacement = 'india')
+cleanset <- tm_map(cleanset, gsub, pattern = 'lok', replacement = 'loksabha')
+cleanset <- tm_map(cleanset, gsub, pattern = 'sabha', replacement = 'loksabha')
+cleanset <- tm_map(cleanset, gsub, pattern = 'modis', replacement = 'modi')
+cleanset <- tm_map(cleanset, removeWords, c('modi' ))
 
 cleanset<- tm_map(cleanset, stripWhitespace)
 inspect(cleanset[1:10])
@@ -37,8 +56,35 @@ tdm[1:10, 1:20]
 #BarPlot
 w<- rowSums(tdm)
 w<- subset(w, w>=15)
-w
+barplot(w, las=2, col= rainbow(50))
+
+#Word Cloud
+library(wordcloud)
+w<- sort(rowSums(tdm), decreasing=TRUE)
+set.seed(222)
+wordcloud(words=names(w), freq=w, max.words = 100, random.order = F, min.freq = 5, colors=brewer.pal(8, 'Dark2'), scale=c(7, 0.3), rot.per = 0.3)
+
+library(wordcloud2)
+w<- data.frame(names(w), w)
+colnames(w)<- c('word', 'freq')
+wordcloud2(w, size=0.5, shape = 'circle', rotateRatio = 0.3, minSize = 1)
 
 
+#Sentiment Analysis
 
+library(syuzhet)
+library(lubridate)
+library(ggplot2)
+library(scales)
+library(reshape2)
+library(dplyr)
+
+PMOTweets <- read.csv(file.choose(), header = T)
+tweets<- iconv(PMOTweets$Text, from = "utf-8", to= "ASCII//TRANSLIT")
+
+#Sentiment scores
+s<- get_nrc_sentiment(tweets)
+
+#bar plot
+barplot(colSums(s), las=2, col=rainbow(10), ylab='Count', main='Sentiment Scores of PMOIndia tweets')
 
